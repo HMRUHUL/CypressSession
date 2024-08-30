e85c8563e98bdfa30f66c8e26354a51308cc7368
ruhulamin731 <ruhul.amin@dsinnovators.com>
2024-08-30 21:05:08 +0600

    done assignment

--- cypress/e2e/sample_web.cy.js
+++ cypress/e2e/sample_web.cy.js
@@ -3,18 +3,35 @@ import { faker } from '@faker-js/faker';
 
 describe('OrangeHRM End to End Testing', () => {
   // const baseUrl = 'https://opensource-demo.orangehrmlive.com/';
+  // Handle uncaught exceptions to prevent test failure
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    console.log('Caught an exception:', err);
+    return false;
+  });
+  cy.waitTillVisible = () => {
+    cy.wait(5000);
+  };
   const adminUserName = "Admin"
   const adminPassword = "admin123"
   console.log(adminPassword)
-
   const employeeDataFile = "employeeData.json" // File to save employee data
   
   function generateRandomPassword() {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    let password = '';
-    for (let i = 0; i < 12; i++) {
-      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    const allChars = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?'
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+    const upper = uppercase.charAt(Math.floor(Math.random() * uppercase.length));
+    const lower = lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+    const num = numbers.charAt(Math.floor(Math.random() * numbers.length));
+    const special = specialChars.charAt(Math.floor(Math.random() * specialChars.length));
+    //let password = '';
+    let password = lower + upper + num + special;
+    for (let i = 5; i < 12; i++) {
+      password += allChars.charAt(Math.floor(Math.random() * allChars.length));
     }
+    password = password.split('').sort(() => 0.5 - Math.random()).join('');
     return password;
   }
 
@@ -23,20 +40,27 @@ describe('OrangeHRM End to End Testing', () => {
     // Before all tests, visit the base URL
     cy.visit('/');
     cy.title().should("eq", "OrangeHRM")
-    // Login using Admin credentials.
+    // Login as Admin
     cy.get("input[name='username']").type(adminUserName)
     cy.get("input[name='password']").type(adminPassword)
     cy.get("[type='submit']").click()
   });
-  // Handle uncaught exceptions to prevent test failure
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    console.log('Caught an exception:', err);
-    return false;
-  });
-
-  cy.waitTillVisible = () => {
-   cy.wait(5000);
-  };
+  function fillUserId() {
+    // const userId = generateRandomUserId();
+    const length = faker.number.int({ min: 1, max: 10 });
+    const userId = faker.string.numeric(length);
+    cy.log(userId);
+    cy.get('label').contains('Employee Id').parent().siblings().find('input').clear()
+        .type(userId)
+    cy.wait(1000);
+    cy.get('body').then(($body) => {
+      if ($body.find('span:contains("Employee Id already exists")').length > 0) {
+        // Retry if the username is not unique
+        cy.log('Employee Id is already taken, generating a new one...');
+        fillUserId();
+      }
+    });
+  }
   it('Validate the whole OrangeHRM flow', () => {
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
 
@@ -48,31 +72,34 @@ describe('OrangeHRM End to End Testing', () => {
     cy.get("button[type='button']").contains("Add").click()
     cy.waitTillVisible('h6')
 
-    // Create a new employee using Faker
+    // Create a New Employee
     const firstName = faker.person.firstName();
+    const middleName = faker.person.middleName();
     const lastName =  faker.person.lastName();
-    const fullName = firstName+" "+lastName;
+    const fName = firstName+" "+middleName+" "+lastName;
+    const fullName = firstName + " "+ lastName;
     cy.log(fullName);
-    // const employeeId = faker.datatype.uuid(); // Using UUID for a unique employee ID
-    // cy.log("EmployeeID",employeeId)
-    const username = firstName + lastName
-    const password = generateRandomPassword()
-    //Fill the employee form
+    const username = firstName + lastName;
+    const password = generateRandomPassword();
     cy.get("input[name='firstName']").type(firstName)
+    cy.get('input[name = "middleName"]').type(middleName);
     cy.get("input[name='lastName']").type(lastName)
-    // Click On Create Login Details toggle
-    cy.get("input[type='checkbox']").click({ force: true })
-    // Fill the login details form.
-    cy.get('label').contains("Username").parent().siblings('div').find('input').type(username)
-    cy.get('label').contains("Password").parent().siblings('div').find('input').type(password)
-    cy.get('label').contains("Confirm Password").parent().siblings('div').find('input').type(password)
+    fillUserId(); //Generate UserId
+    // Create Login Details
+    cy.get("input[type='checkbox']").click({ force: true });
+    cy.get('label').contains("Username").parent().siblings('div').find('input')
+        .type(username)
+    cy.get('label').contains("Password").parent().siblings('div').find('input')
+        .type(password)
+    cy.get('label').contains("Confirm Password").parent().parent().children().find('input')
+        .type(password)
     cy.get("button[type='submit']").click()
     // Assert the Employee Creation Notification Message
     cy.get('.oxd-text--toast-message').should("have.text", "Successfully Saved")
     cy.waitTillVisible('h6')
     cy.get('h6').should("contain.text", fullName)
     //
-    // Save Employee Details to a file
+    // Write file
     cy.writeFile(`cypress/fixtures/${employeeDataFile}`,{
       username,
       password
@@ -82,20 +109,18 @@ describe('OrangeHRM End to End Testing', () => {
     cy.get("span").contains("Directory").click()
     cy.waitTillVisible("h5")
     // Type the firstName in the Employee Name
-    cy.get("input[placeholder='Type for hints...']").type(firstName)
-    // Get the locator from the API response from the cypress execution snapshot
+    // cy.get("input[placeholder='Type for hints...']").type(firstName)
+    cy.get('label').contains('Employee Name').parent().siblings().find('input')
+        .type(firstName);
+    cy.wait(2000);
     cy.get('.oxd-autocomplete-option > span').click()
     // Click on Search
     cy.get("button[type='submit']").click()
-    // Assert employee name after searching the employee in the Directory
-    // Will fail due to extra spaces in the name, need to trim it.
-    // cy.get(".orangehrm-directory-card-header").should("have.text",firstName + " " + lastName)
-
     cy.get(".orangehrm-directory-card-header")
       .invoke('text')
       .then((text) => {
         const normalizedText = text.replace(/\s+/g, ' ').trim(); // Collapse multiple spaces into one and trim
-        expect(normalizedText).to.eq(fullName);
+        expect(normalizedText).to.eq(fName);
       });
 
     // Log Out As an admin.
