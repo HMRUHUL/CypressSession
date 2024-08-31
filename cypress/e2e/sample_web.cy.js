ddf93653069c8488625c0e3a642e54db36139efa
ruhulamin731 <ruhul.amin@dsinnovators.com>
2024-08-31 23:12:48 +0600

    Assignment Done

--- cypress/e2e/sample_web.cy.js
+++ cypress/e2e/sample_web.cy.js
@@ -34,18 +34,18 @@ describe('OrangeHRM End to End Testing', () => {
     return password;
   }
   // Hooks
-  before(() => {
+  beforeEach(() => {
     // Before all tests, visit the base URL
     cy.visit('/');
     cy.title().should("eq", "OrangeHRM")
-    // Login as Admin
-    cy.get("input[name='username']").type(adminUserName)
-    cy.get("input[name='password']").type(adminPassword)
-    cy.get("[type='submit']").click()
+    cy.waitTillVisible()
   });
+  let userId = '';
+  let fullName ='';
+  let password = '';
   function fillUserId() {
     const length = faker.number.int({ min: 1, max: 10 });
-    const userId = faker.string.numeric(length);
+    userId = faker.string.numeric(length);
     cy.log(userId);
     cy.get('label').contains('Employee Id').parent().siblings().find('input').clear()
         .type(userId)
@@ -57,8 +57,12 @@ describe('OrangeHRM End to End Testing', () => {
       }
     });
   }
-  it('Validate the whole OrangeHRM flow', () => {
+
+  it('OrangeHRM flow for Admin', () => {
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+    cy.get("input[name='username']").type(adminUserName)
+    cy.get("input[name='password']").type(adminPassword)
+    cy.get("[type='submit']").click()
 
     cy.waitTillVisible('h6')
     cy.get('h6').should("have.text", "Dashboard")
@@ -67,16 +71,19 @@ describe('OrangeHRM End to End Testing', () => {
     //click on Add Button
     cy.get("button[type='button']").contains("Add").click()
     cy.waitTillVisible('h6')
+    // cy.get('.employee-image').click() //parent().siblings().find('button i')
+    // cy.get('.employee-image')
+    //     .attachFile('images.jpg');
 
     // Create a New Employee
     const firstName = faker.person.firstName();
     const middleName = faker.person.middleName();
     const lastName =  faker.person.lastName();
     const fName = firstName+" "+middleName+" "+lastName;
-    const fullName = firstName + " "+ lastName;
+    fullName = firstName + " "+ lastName;
     cy.log(fullName);
     const username = firstName + lastName;
-    const password = generateRandomPassword();
+    password = generateRandomPassword();
     cy.get("input[name='firstName']").type(firstName)
     cy.get('input[name = "middleName"]').type(middleName);
     cy.get("input[name='lastName']").type(lastName)
@@ -90,16 +97,30 @@ describe('OrangeHRM End to End Testing', () => {
     cy.get('label').contains("Confirm Password").parent().parent().children().find('input')
         .type(password)
     cy.get("button[type='submit']").click()
+    // Write file
+    cy.writeFile(`cypress/fixtures/${employeeDataFile}`,{
+      userId,
+      username,
+      password
+    });
     // Assert the Employee Creation Notification Message
     cy.get('.oxd-text--toast-message').should("have.text", "Successfully Saved")
     cy.waitTillVisible('h6')
     cy.get('h6').should("contain.text", fullName)
-    //
-    // Write file
-    cy.writeFile(`cypress/fixtures/${employeeDataFile}`,{
-      username,
-      password
-    });
+    //Search by Employee
+    cy.get(".oxd-main-menu-item.active").contains("PIM").click()
+    cy.get('label').contains('Employee Id').parent().siblings().find('input')
+        .type(userId);
+    cy.get("button[type='submit']").click()
+    cy.wait(5000);
+    cy.get(".oxd-table-cell")
+        .invoke('text')
+        .then(text => {
+          const uId = text.match(/\d+/g);
+          expect(uId[0]).to.eq(userId);
+        })
+
+
 
     // Click On the Directory Menu
     cy.get("span").contains("Directory").click()
@@ -118,21 +139,38 @@ describe('OrangeHRM End to End Testing', () => {
         const normalizedText = text.replace(/\s+/g, ' ').trim(); // Collapse multiple spaces into one and trim
         expect(normalizedText).to.eq(fName);
       });
-
     // Log Out As an admin.
     cy.get("span img").click()
     cy.get("li a").contains("Logout").click()
-    cy.waitTillVisible("h5")
-
-    // Log In Using Newly Created Employee Creds
-    cy.fixture(employeeDataFile).then((employee)=>{
-      cy.get("input[name='username']").type(employee.username)
-      cy.get("input[name='password']").type(employee.password)
-      cy.get("[type='submit']").click()
-      cy.get("p.oxd-userdropdown-name").should("have.text",fullName)
-    })
+    cy.log(fullName)
   });
 
+ it('User Login', () =>{
+   cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+   cy.log(fullName)
+   cy.fixture(employeeDataFile).then((employee)=>{
+     cy.get("input[name='username']").type(employee.username)
+     cy.get("input[name='password']").type(employee.password)
+     cy.get("[type='submit']").click()
+     // cy.get("p.oxd-userdropdown-name").should("have.text",fullName)
+     //Update My Info
+     cy.get("span").contains("My Info").click()
+     cy.waitTillVisible();
+     cy.get('label').contains('Gender')
+         .parent().siblings().find('input[type="radio"][value="1"]').click({force:true});
+     cy.get('button[type="submit"]').first().click();
+     cy.get('.oxd-text--toast-message').should("have.text", "Successfully Updated")
+     cy.waitTillVisible()
+
+     cy.get('label').contains('Blood Type')
+         .parent().siblings().find('.oxd-select-text-input').click()
+     cy.contains('O+').click();
+     cy.get('button[type="submit"]').eq(1).click();
+     cy.get('.oxd-text--toast-message').should("have.text", "Successfully Saved")
+     cy.waitTillVisible()
+
+   })
+  })
   after(() => {
     // Log Out As Newly Created Employee.
     cy.get("span img").click()
